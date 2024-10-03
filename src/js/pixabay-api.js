4c174b0fd97355184087cdab7a5a717fe5ae4087
waiolet <violet.bondarenko@gmail.com>
2024-10-03 22:15:06 +0200

    modify js

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+export function fetchData(query) {
+    const searchParams = new URLSearchParams({
+        key: "46332021-f156325159d295bd8ceb1a335",
+        q: `${inputSearch}`,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+    });
+    return fetch(
+        `https://pixabay.com/api/?${searchParams}`
+    ).then((response) => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    }); 
+}
