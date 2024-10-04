45b6cf7e79339587d316a63a1433c77c38566f30
waiolet <violet.bondarenko@gmail.com>
2024-10-04 11:31:02 +0200

    modify

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,40 +1,43 @@
 export function renderData(imgArr, userList) {
-    const markup = imgArr.map(({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-    }) => {
-        return `<li class="gallery-list-item">
-                    <a class="gallery-link" href="${largeImageURL}">
-                        <img class="img" src="${webformatURL}" 
-                        alt="${tags}" 
-                        title="${tags}" />
-                    </a>
-                    <ul class="data-list">
-                        <li class="data-item">
-                            <p class="data-item-name">Likes</p>
-                            <p class="data-numbers">${likes}</p>
-                        </li>
-                        <li class="data-item">
-                            <p class="data-item-name">Views</p>
-                            <p class="data-numbers">${views}</p>
-                        </li>
-                        <li class="data-item">
-                            <p class="data-item-name">Comments</p>
-                            <p class="data-numbers">${comments}</p>
-                        </li>
-                        <li class="data-item">
-                            <p class="data-item-name">Downloads</p>
-                            <p class="data-numbers">${downloads}</p>
-                        </li>
-                    </ul> 
-          </li>`;
-    }).join("");
+    if (!Array.isArray(imgArr) || imgArr.length === 0) {
+        console.warn('No images to render.');
+        return; 
+    }
 
-    
-    userList.insertAdjacentHTML("beforeend", markup);
+    const fragment = document.createDocumentFragment();
+
+    imgArr.forEach(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        const listItem = document.createElement('li');
+        listItem.className = 'gallery-list-item';
+
+        listItem.innerHTML = `
+            <a class="gallery-link" href="${largeImageURL}" aria-label="${tags}">
+                <img class="img" src="${webformatURL}" 
+                alt="${tags}" 
+                title="${tags}" />
+            </a>
+            <ul class="data-list">
+                <li class="data-item">
+                    <p class="data-item-name">Likes</p>
+                    <p class="data-numbers">${likes}</p>
+                </li>
+                <li class="data-item">
+                    <p class="data-item-name">Views</p>
+                    <p class="data-numbers">${views}</p>
+                </li>
+                <li class="data-item">
+                    <p class="data-item-name">Comments</p>
+                    <p class="data-numbers">${comments}</p>
+                </li>
+                <li class="data-item">
+                    <p class="data-item-name">Downloads</p>
+                    <p class="data-numbers">${downloads}</p>
+                </li>
+            </ul>
+        `;
+
+        fragment.appendChild(listItem);
+    });
+
+    userList.appendChild(fragment);
 }
