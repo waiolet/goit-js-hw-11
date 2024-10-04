939812f1de5ae693f848b0e6ba5a1bfa6fdf0775
waiolet <violet.bondarenko@gmail.com>
2024-10-04 11:53:55 +0200

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,44 +1,44 @@
-export function handleSuccess(data) {
-  return data
-    .map(
-      ({
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<li class="gallery-item">
-        <article class="card">
-    <a class="card-link" href="${largeImageURL}"><img class="card-image" src="${webformatURL}" alt="${tags}" loading="lazy"/></a>        <div class="card-container">
-          <div class="card-item">
-            <p class="card-title">Likes</p>
-            <p class="card-count">${likes}</p>
-          </div>
-          <div class="card-item">
-            <p class="card-title">Views</p>
-            <p class="card-count">${views}</p>
-          </div>
-          <div class="card-item">
-            <p class="card-title">Comments</p>
-            <p class="card-count">${comments}</p>
-          </div>
-          <div class="card-item">
-            <p class="card-title">Downloads</p>
-            <p class="card-count">${downloads}</p>
-          </div>
-        </div>
-        </article>
-          </li>`;
-      }
-    )
-    .join('');
-}
-
 export function handleSuccess(images) {
-    return renderData(images, document.querySelector('.gallery'));
+    const markup = images.map(
+        ({
+            largeImageURL,
+            webformatURL,
+            tags,
+            likes,
+            views,
+            comments,
+            downloads,
+        }) => {
+            return `<li class="gallery-item">
+            <article class="card">
+                <a class="card-link" href="${largeImageURL}">
+                    <img class="card-image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+                </a>
+                <div class="card-container">
+                    <div class="card-item">
+                        <p class="card-title">Likes</p>
+                        <p class="card-count">${likes}</p>
+                    </div>
+                    <div class="card-item">
+                        <p class="card-title">Views</p>
+                        <p class="card-count">${views}</p>
+                    </div>
+                    <div class="card-item">
+                        <p class="card-title">Comments</p>
+                        <p class="card-count">${comments}</p>
+                    </div>
+                    <div class="card-item">
+                        <p class="card-title">Downloads</p>
+                        <p class="card-count">${downloads}</p>
+                    </div>
+                </div>
+            </article>
+            </li>`;
+        }
+    ).join('');
+
+    const gallery = document.querySelector('.gallery');
+    gallery.insertAdjacentHTML('beforeend', markup);
 }
 
 export function showLoader(loaderIndicator) {
@@ -50,4 +50,3 @@ export function hideLoader(loaderIndicator) {
     loaderIndicator.classList.add('hidden');
     loaderIndicator.style.display = 'none';
 }
-
