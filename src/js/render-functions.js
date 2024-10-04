3573496f4e2c783f6ba382fe20741062b278c4e6
waiolet <violet.bondarenko@gmail.com>
2024-10-04 11:51:23 +0200

    mod

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,45 +1,40 @@
-export function renderData(imgArr, userList) {
-    if (!Array.isArray(imgArr) || imgArr.length === 0) {
-        console.warn('No images to render.');
-        return; 
-    }
-
-    const fragment = document.createDocumentFragment();
-
-    imgArr.forEach(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-        const listItem = document.createElement('li');
-        listItem.className = 'gallery-list-item';
-
-        listItem.innerHTML = `
-            <a class="gallery-link" href="${largeImageURL}" aria-label="${tags}">
-                <img class="img" src="${webformatURL}" 
-                alt="${tags}" 
-                title="${tags}" />
-            </a>
-            <ul class="data-list">
-                <li class="data-item">
-                    <p class="data-item-name">Likes</p>
-                    <p class="data-numbers">${likes}</p>
-                </li>
-                <li class="data-item">
-                    <p class="data-item-name">Views</p>
-                    <p class="data-numbers">${views}</p>
-                </li>
-                <li class="data-item">
-                    <p class="data-item-name">Comments</p>
-                    <p class="data-numbers">${comments}</p>
-                </li>
-                <li class="data-item">
-                    <p class="data-item-name">Downloads</p>
-                    <p class="data-numbers">${downloads}</p>
-                </li>
-            </ul>
-        `;
-
-        fragment.appendChild(listItem);
-    });
-
-    userList.appendChild(fragment);
+export function handleSuccess(data) {
+  return data
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<li class="gallery-item">
+        <article class="card">
+    <a class="card-link" href="${largeImageURL}"><img class="card-image" src="${webformatURL}" alt="${tags}" loading="lazy"/></a>        <div class="card-container">
+          <div class="card-item">
+            <p class="card-title">Likes</p>
+            <p class="card-count">${likes}</p>
+          </div>
+          <div class="card-item">
+            <p class="card-title">Views</p>
+            <p class="card-count">${views}</p>
+          </div>
+          <div class="card-item">
+            <p class="card-title">Comments</p>
+            <p class="card-count">${comments}</p>
+          </div>
+          <div class="card-item">
+            <p class="card-title">Downloads</p>
+            <p class="card-count">${downloads}</p>
+          </div>
+        </div>
+        </article>
+          </li>`;
+      }
+    )
+    .join('');
 }
 
 export function handleSuccess(images) {
